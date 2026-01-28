540142c609e438b7ad0d82eaf57d0a8af2653bb3
MLML8 <jacky55668884@gmail.com>
2026-01-28 18:12:23 -0500

    Inital commit with Hello World in the extension

---
+++ README.md
@@ -0,0 +1,7 @@
+How to get started:
+
+Make sure npm is installed on you computer
+
+In this directory do: npm install
+
+To run it do: npm run dev
---
+++ wxt.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ['@wxt-dev/module-react'],
+  manifest: ({ browser, manifestVersion, mode, command }) => {
+      return {
+          manifest_version: 3,
+          name: "TimeTracker",
+          description: "Tracks Time",
+          permissions: ["storage"],
+      };
+  },
+});
