9a93b5600f7bdb03a3f6592b112670fc4839adc9
draveniii <draveniii@gmail.com>
2026-04-07 01:05:59 -0400

    Added Alarms background service and Alarms page.

---
+++ entrypoints/popup/services/Alarm.ts
@@ -0,0 +1,46 @@
+export interface Alarm {
+  domain: string;
+  currentTime: number;
+  duration: number;
+}
+
+export const alarms = storage.defineItem<Record<string, Alarm>>('local:alarms', {
+  fallback: {},
+});
+
+export async function getAllAlarms() {
+  try {
+    const data = await alarms.getValue();
+    return data;
+  } catch (error) {
+    console.error("Error getting alarms:", error);
+    return [];
+  }
+}
+
+export async function createAlarm(alarmId: string, alarm: Alarm) {
+  try {
+    const allAlarms = await alarms.getValue();
+    if (!allAlarms[alarmId]) {
+      allAlarms[alarmId] = alarm;
+      await alarms.setValue(allAlarms);
+    }
+    return true;
+  } catch (error) {
+    console.error("Error adding alarm:", error);
+    return false;
+  }
+}
+
+export async function deleteAlarm (alarmId: string) {
+  try {
+      const allAlarms = await alarms.getValue();
+      delete allAlarms[alarmId];
+      await alarms.setValue(allAlarms);
+      console.log("Deleted alarm with ID:", alarmId);
+      return true;
+    } catch (error) {
+      console.error("Error deleting alarm:", error);
+      return false;
+    }
+}
